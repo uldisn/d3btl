4a00524fc777c72cc1b7bc097af92c73ab51dc1e
Uldis Nelsons <uldis@weberp.lv>
2021-12-10 11:19:11 +0200

    piemers

--- models/BtlFileData.php
+++ models/BtlFileData.php
@@ -9,5 +9,54 @@ use d3yii2\d3btl\models\base\BtlFileData as BaseBtlFileData;
  */
 class BtlFileData extends BaseBtlFileData
 {
+  
+  public $in = [];
+ 
+  public function load($data)
+  {
+    if (!parent::load($data)) {
+      return false;
+    }
+    if($this->file_data && !$ths->processed_data) {
+      $this->processFile()
+    }
+    rweturn true;
+  }
+  
+  public function processFile()
+  {
+
+    $this->processed_data = 'aaaa'
+    $in = BtlPart()  
+    $in->????
+    $this->in[] = $in;  
+
+    $out = BtlPart()  
+    $out->????
+    $this->out[] = $out;  
+  }
+  
+  public function save(???)
+  {
+    if (!$this->save()){
+      return false;
+    }
+    
+    foreach($this->in as $in) {
+      $in->file_data_id = $this->id;
+         if (!$in->save()){
+           return false;
+      } 
+    }
+    foreach($this->out as $out) {
+      $out->file_data_id = $this->id;
+         if (!$out->save()){
+           return false;
+      } 
+    }
+    return true;
+  }
+  
+  public function
 
 }
